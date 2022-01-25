100bb856c90f334a8561faa8a6df9315ce6bc105
GULTOM <firebasegultom@gmail.com>
2022-01-25 10:56:23 +0700

    update dashboard admin and user

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -19,7 +19,6 @@
                                     <th>Price</th>
                                     <th>Register Date</th>
                                     <th>Paid Status</th>
-                                    <th>Action</th>
                                </tr>
                             </thead>
                             <tbody>
@@ -30,19 +29,7 @@
                                     <td>{{ $checkout->Camp->price }}</td>
                                     <td>{{ $checkout->created_at->format('M d Y') }}</td>
                                     <td>
-                                        @if ($checkout->is_paid)
-                                        <span class="badge bg-success">Paid</span>
-                                        @else
-                                        <span class="badge bg-warning">Waiting</span>                                          
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <form action="{{ route('admin.checkout.update', $checkout->id) }}" method="post">
-                                            @if (!$checkout->is_paid)
-                                            @csrf
-                                            <button class="btn btn-primary btn-sm">Set to Paid</button>
-                                            @endif
-                                        </form>
+                                        <strong>{{ $checkout->payment_status }}</strong>
                                     </td>
                                 </tr>
                                 @empty
